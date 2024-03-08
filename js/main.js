d09426471734bb36281ecef6d3136ff5a8723f71
Amit Kumar <amitk6228@gmail.com>
2024-03-08 16:31:06 +0530

    first

---
+++ js/main.js
@@ -0,0 +1,93 @@
+(function ($) {
+    "use strict";
+
+    /*==================================================================
+    [ Validate ]*/
+    var input = $('.validate-input .input100');
+
+    $('.validate-form').on('submit', function () {
+        var check = true;
+
+        for (var i = 0; i < input.length; i++) {
+            if (validate(input[i]) == false) {
+                showValidate(input[i]);
+                check = false;
+            }
+        }
+
+        return check;
+    });
+
+
+    $('.validate-form .input100').each(function () {
+        $(this).focus(function () {
+            hideValidate(this);
+        });
+    });
+
+    function validate(input) {
+        if ($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+            if ($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+                return false;
+            }
+        } else {
+            if ($(input).val().trim() == '') {
+                return false;
+            }
+        }
+    }
+
+    function showValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).addClass('alert-validate');
+    }
+
+    function hideValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).removeClass('alert-validate');
+    }
+
+    /*==================================================================
+    [ Modal ]*/
+    $('.modal-subscribe').on('click', function (e) {
+        e.stopPropagation();
+    })
+
+    $('.btn-close-modal').on('click', function () {
+        $('#subscribe').modal('hide');
+    });
+
+    /*==================================================================
+    [ Countdown ]*/
+    var endDate = new Date("April 8, 2024 00:00:00");
+    var now = new Date();
+    var timeLeft = endDate - now;
+
+    function updateCountdown() {
+        var days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+        $('.days').text(days);
+        $('.hours').text(hours);
+        $('.minutes').text(minutes);
+        $('.seconds').text(seconds);
+
+        if (timeLeft <= 0) {
+            clearInterval(interval);
+            // Do something when countdown ends
+            alert('Countdown ended!');
+        }
+    }
+
+    updateCountdown(); // Initial call to set the countdown
+
+    var interval = setInterval(function () {
+        timeLeft -= 1000; // Decrease 1 second every second
+        updateCountdown();
+    }, 1000);
+
+})(jQuery);
